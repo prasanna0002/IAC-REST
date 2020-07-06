1d13fd7307ba433529b71346ef1cc407c05fca7d
prasanna0002 <52502852+prasanna0002@users.noreply.github.com>
2020-07-06 21:22:25 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-TASK_FAMILY= "maven"
-SERVICE_NAME= "maven"
-NEW_DOCKER_IMAGE="prasanna002/maven:${GITHUB_RUN_NUMBER}"
-CLUSTER_NAME= "Terraform_Docker_Batch_112d5611-fb75-38bd-8843-bbc001e83301"
+TASK_FAMILY= "first-run-task-definition"
+SERVICE_NAME= "maven-service"
+NEW_DOCKER_IMAGE="prasanna002/maven:latest"
+CLUSTER_NAME= "default"
 OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --output json)
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
